6601b423cb9b8d41886e0d72b931328f1fd46e71
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2018-12-19 21:26:34 -0800

    created and a key and a value

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
+  //var values = [];
 
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+    //var myItemInStorage = localStorage.getItem($('.items').val());
+    var item = $('.items').val()
+    var price = $('.price').val()
+    // localStorage.setItem(($('.items').val()), $('.price').val()); works *
+    localStorage.setItem(item, price)
+    //values.push($('.text-entry').val())
+    //console.log(values)
+    
+    //console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
-
+    // $('.display').append('<div>' + item + " " + price + '</div>'); // ??
+    $('.display').text(item + " " + price);
   });
+  
+  // $('.btn-submit').on('click', function(){
 
+  //   localStorage.setItem('inputFieldValue', $('.text-entry').val());
+  //   var myItemInStorage = localStorage.getItem('inputFieldValue');
+  //   console.log('myItemInStorage', myItemInStorage);
+
+  //   // display the value here
+  //   $('.list-display-field').text(myItemInStorage); // ??
+
+  // });
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
     //$('.text-entry').empty();
-    $('.list-display-field').empty();
+    $('.display').empty();
     localStorage.removeItem('inputFieldValue');
   });
 
